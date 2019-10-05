12074ea3b67977a5a78d2e809295e9fa9bfcd26d
Jamie Cansdale <jcansdale@github.com>
2019-10-05 12:32:33 +0100

    Create DogfoodService using reflection
    We can't reference the 16.0 assembly because it targets .NET 4.7.2.

--- src/Dogfood.Services/DogfoodService.cs
+++ src/Dogfood.Services/DogfoodService.cs
@@ -1,16 +1,10 @@
-﻿extern alias DS14;
-extern alias DS15;
-extern alias DS16;
-using System;
+﻿using System;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.ComponentModel.Composition;
 using Dogfood.Exports;
 using EnvDTE;
-using DogfoodService14 = DS14::Dogfood.Services.DogfoodService;
-using DogfoodService15 = DS15::Dogfood.Services.DogfoodService;
-using DogfoodService16 = DS16::Dogfood.Services.DogfoodService;
 
 namespace Dogfood.Services
 {
@@ -38,25 +32,23 @@ namespace Dogfood.Services
         public Task<bool> Reinstall(string vsixFile, IProgress<string> progress) =>
             dogfoodService.Value.Reinstall(vsixFile, progress);
 
-        static IDogfoodService CreateDogfoodService(DTE dte, IServiceProvider serviceProvider,
-            IProjectUtilities projectUtilities)
+        IDogfoodService CreateDogfoodService(DTE dte, IServiceProvider serviceProvider, IProjectUtilities projectUtilities)
         {
             switch (dte.Version)
             {
                 case "14.0":
-                    return CreateDogfoodService(() => new DogfoodService14(serviceProvider, projectUtilities));
                 case "15.0":
-                    return CreateDogfoodService(() => new DogfoodService15(serviceProvider, projectUtilities));
+                    return CreateDogfoodService(dte.Version, serviceProvider, projectUtilities);
                 case "16.0":
                     try
                     {
                         Assembly.Load("Microsoft.VisualStudio.ExtensionManager, Version=16.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-                        return CreateDogfoodService(() => new DogfoodService16(serviceProvider, projectUtilities));
+                        return CreateDogfoodService(dte.Version, serviceProvider, projectUtilities);
                     }
                     catch (FileNotFoundException)
                     {
                         // If we can't load ExtensionManager 16.0, fall back to using the 15.0 version
-                        return CreateDogfoodService(() => new DogfoodService15(serviceProvider, projectUtilities));
+                        return CreateDogfoodService("15.0", serviceProvider, projectUtilities);
                     }
 
                 default:
@@ -64,6 +56,19 @@ namespace Dogfood.Services
             }
         }
 
-        static IDogfoodService CreateDogfoodService(Func<IDogfoodService> factory) => factory();
+        IDogfoodService CreateDogfoodService(string version, IServiceProvider serviceProvider, IProjectUtilities projectUtilities)
+        {
+            var executingType = GetType();
+            var fullName = executingType.FullName;
+            var assemblyName = executingType.Assembly.GetName();
+            assemblyName.Name = $"{assemblyName.Name}.{version}";
+            var assemblyQualifiedName = $"{fullName}, {assemblyName}";
+            if (Type.GetType(assemblyQualifiedName, false) is Type targetType)
+            {
+                return (IDogfoodService)Activator.CreateInstance(targetType, serviceProvider, projectUtilities);
+            }
+
+            return null;
+        }
     }
 }
